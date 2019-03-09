27faffbc70e00cbd5527ba05f266b35c5acc2fb5
Selman Samet <selmansametun@gmail.com>
2019-03-09 21:38:42 +0300

    p5 class execute when click the run button

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 function executeScript(source) {
-    console.log(source)
     var script = document.createElement("script");
-    script.onload = script.onerror = function(){ this.remove(); };
+    script.onerror = function(){ this.remove(); };
+    script.onload = function(){ this.remove();  }
     script.src = "data:text/plain;base64," + btoa(source);
     document.body.appendChild(script);
 }
 
 (function(){
     document.querySelector('#run').onclick = function(){
+        if(window.p){
+            window.p.remove();
+            window.p = null;
+        }
         const value = window.editor.getValue();
-        var p = new p5()
-        p.canvas = document.getElementById('canvas');
+        window.p = new p5()
         executeScript(value);
     }
 })();
